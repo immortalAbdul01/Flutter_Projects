90929126b73ae44e510fbb717c4fbd2b9ac99e43
Immortal <ak2061435@gmail.com>
2023-05-22 08:56:37 +0530

    widget change

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:expense_tracker/models/expenses_model.dart';
+
+final formatter = DateFormat.yMMMEd();
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -12,6 +16,23 @@ class NewExpense extends StatefulWidget {
 class _NewExpense extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  Category _selectedCategory = Category.food;
+
+  void _openDatePicker() async {
+    final now = DateTime.now();
+    final startDate = DateTime(now.year - 1, now.month, now.day);
+    // final endDate = DateTime(now.year);
+    final pickedDate = await showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: startDate,
+        lastDate: now);
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose() {
@@ -31,16 +52,57 @@ class _NewExpense extends State<NewExpense> {
             maxLength: 50,
             decoration: const InputDecoration(label: Text('Title')),
           ),
-          TextField(
-            maxLength: 10,
-            keyboardType: TextInputType.number,
-            controller: _amountController,
-            decoration:
-                const InputDecoration(label: Text('Amount'), prefixText: 'RS '),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  maxLength: 10,
+                  keyboardType: TextInputType.number,
+                  controller: _amountController,
+                  decoration: const InputDecoration(
+                      label: Text('Amount'), prefixText: 'RS '),
+                ),
+              ),
+              Expanded(
+                  child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(_selectedDate == null
+                      ? 'No date selected'
+                      : formatter.format(_selectedDate!)),
+                  IconButton(
+                      onPressed: _openDatePicker,
+                      icon: const Icon(Icons.calendar_month))
+                ],
+              ))
+            ],
+          ),
+          const SizedBox(
+            height: 18,
           ),
           Row(
             children: [
-              TextButton(onPressed: () {}, child: const Text('cancel')),
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map((cat) => DropdownMenuItem(
+                          value: cat, child: Text(cat.name.toUpperCase())))
+                      .toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      if (value == null) {
+                        return;
+                      }
+                      _selectedCategory = value;
+                    });
+                  }),
+              const Spacer(),
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('cancel')),
               ElevatedButton(
                   onPressed: () {
                     print(_titleController.text);
